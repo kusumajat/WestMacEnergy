8de51c0bb389ea9217260c2f9fa4ee7c1bb9d471
Kusumajati <rkusumajati14@gmail.com>
2025-06-22 00:03:12 +0700

    home-page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,16 @@
 @section('title', 'Home') {{-- Menetapkan judul khusus untuk halaman ini --}}
 
 @section('content')
-    <section class="hero" style="background-image: url('images/img_bg_1.jpg');">
+    <section class="hero" style="background-image: url('images/mine1.png');">
         <div class="overlay">
             <div class="hero-content">
-                <h1 class="headline">Ecommerce</h1>
+                <h1 class="headline">WestMacEnergy</h1>
             </div>
         </div>
     </section>
     <section class="tagline">
         <div class="container-fluid">
-            <h2>shipping made easy.</h2>
+            <h2>West Macedonia Lignite Centre</h2>
         </div>
     </section>
 
@@ -21,38 +21,33 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="about-img animate-box" data-animate-effect="fadeInLeft"
-                        style="background-image: url(images/img_bg_2.jpg);">
+                        style="background-image: url(images/mine2.png);">
                     </div>
                 </div>
                 <div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
                     <div class="about-desc">
                         <span class="heading-meta">Welcome</span>
                         <h2 class="colorlib-heading">Who we are</h2>
-                        <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came
-                            from it would have been rewritten a thousand times and everything that was left from
-                            its origin would be the word "and" and the Little Blind Text should turn around and
-                            return to its own, safe country.</p>
-                        <p>A small river named Duden flows by their place and supplies it with the necessary
-                            regelialia. It is a paradisematic country, in which roasted parts of sentences fly
-                            into your mouth.</p>
+                        <p>For decades, the West Macedonia Lignite Centre (WMLC) has been the heart of Greek energy. Operated by PPC S.A., our mines have reliably fueled the nation's progress as one of Europe's largest lignite producers.</p>
+                        <p>Now, as the world embraces cleaner energy, we are leading Greece's Just Transition. We leverage our deep expertise to transform our legacy, revitalizing the land and developing new opportunities in renewable energy for a sustainable future.</p>
                     </div>
                     <div class="row padding">
                         <div class="col-md-4 no-gutters animate-box" data-animate-effect="fadeInLeft">
                             <a href="#" class="steps active">
                                 <p class="icon"><span><i class="icon-check"></i></span></p>
-                                <h3>We are <br>pasionate</h3>
+                                <h3>Respecting <br>Our Earth</h3>
                             </a>
                         </div>
                         <div class="col-md-4 no-gutters animate-box" data-animate-effect="fadeInLeft">
                             <a href="#" class="steps">
                                 <p class="icon"><span><i class="icon-check"></i></span></p>
-                                <h3>Honest <br>Dependable</h3>
+                                <h3>Future <br>Focused</h3>
                             </a>
                         </div>
                         <div class="col-md-4 no-gutters animate-box" data-animate-effect="fadeInLeft">
                             <a href="#" class="steps">
                                 <p class="icon"><span><i class="icon-check"></i></span></p>
-                                <h3>Always <br>Improving</h3>
+                                <h3>Innovating <br>Forward</h3>
                             </a>
                         </div>
                     </div>
