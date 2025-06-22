59bd74e524e369ef0762a16e7ea8487b1cd2a431
Kusumajati <rkusumajati14@gmail.com>
2025-06-22 14:36:44 +0700

    ver

--- api/index.php
+++ api/index.php
@@ -1,2 +1,13 @@
 <?php
+
+// Vercel PHP bootstrap
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
+}
+
+// Set correct path for Laravel
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+// Include Laravel's public index
 require __DIR__ . '/../public/index.php';
